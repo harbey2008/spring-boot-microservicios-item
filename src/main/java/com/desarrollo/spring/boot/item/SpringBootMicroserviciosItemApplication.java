623e6774c1758fd88761218d186486454cfd337b
harbey2008 <harbey2008@hotmail.com>
2021-03-16 13:52:30 -0500

    Proyecto MicroServicios Item Producto

---
+++ src/main/java/com/desarrollo/spring/boot/item/SpringBootMicroserviciosItemApplication.java
@@ -0,0 +1,13 @@
+package com.desarrollo.spring.boot.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviciosItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviciosItemApplication.class, args);
+	}
+
+}
